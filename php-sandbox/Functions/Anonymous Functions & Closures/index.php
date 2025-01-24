c13a8a8b795513d643dd4b38f1d64aa52dd42d26
Teodora Marian <t.a.marian@student.rug.nl>
2025-01-24 15:24:49 +0200

    Lecture 8 Section 4

---
+++ php-sandbox/Functions/Anonymous Functions & Closures/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$square = function ($number) {
+    return $number * $number;
+};
+
+$result = $square(5);
+
+echo $result;
+
+
+//Closures
+
+function createCounter()
+{
+    $count = 0;
+    $counter = function () use (&$count) {
+        return ++$count;
+    };
+    return $counter;
+}
+
+$counter = createCounter();
+echo $counter();
