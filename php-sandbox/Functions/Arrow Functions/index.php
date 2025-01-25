8ccc801ca1f78a0435b00ba433725e454fe6d372
Teodora Marian <t.a.marian@student.rug.nl>
2025-01-25 11:24:58 +0200

    Section Functions done

---
+++ php-sandbox/Functions/Arrow Functions/index.php
@@ -0,0 +1,10 @@
+<?php
+
+$add = fn($a, $b) => $a + $b; //In PHP they can only be one-liners
+
+echo $add(1, 2);
+
+$numbers = [1, 2, 3, 4, 5];
+$squaredNumbers = array_map(fn($number) => $number * $number, $numbers);
+
+print_r($squaredNumbers);
