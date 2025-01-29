94f93cd39696969ca65757a76b84ef342472e046
Teodora Marian <t.a.marian@student.rug.nl>
2025-01-29 13:41:41 +0200

    SESSION

---
+++ php-sandbox/Superglobals/$_SESSION/index.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+$_SESSION['name'] = 'John';
+print_r($_SESSION);
+
+if (isset($_SESSION['name'])) {
+    echo 'Name: ' . $_SESSION['name'];
+} else {
+    echo 'Name aint set';
+}
