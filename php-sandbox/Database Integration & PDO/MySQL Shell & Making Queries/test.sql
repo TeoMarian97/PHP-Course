8c17b990f756b42f2df3f9985387ae212b5379ea
Teodora Marian <t.a.marian@student.rug.nl>
2025-01-30 18:17:25 +0200

    New section slay queen

---
+++ php-sandbox/Database Integration & PDO/MySQL Shell & Making Queries/test.sql
@@ -0,0 +1,9 @@
+CREATE TABLE posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+INSERT INTO posts (title, body) VALUES ('Post One', 'This is our first post');
