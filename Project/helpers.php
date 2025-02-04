da9cb0a28c5dc2063f1431c3ef2d5961142496a3
Teodora Marian <t.a.marian@student.rug.nl>
2025-02-04 16:13:26 +0200

    made some partials

--- Project/helpers.php
+++ Project/helpers.php
@@ -9,3 +9,33 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * @param string $name
+ * @return void
+ */
+function loadView($name)
+{
+    $viewPath = basePath("Views/{$name}.view.php");
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View '{$viewPath} not found";
+    }
+}
+
+/**
+ * Load partials
+ * @param string $name
+ * @return void
+ */
+function loadPartial($name)
+{
+    $partialPath = basePath("Views/Partials/{$name}.php");
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "View '{$partialPath} not found";
+    }
+}
